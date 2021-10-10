928f5b163834429433bb24633c5eb551b1d66947
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-10 20:06:11 +0800

    Demo App

---
+++ demo/DemoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using CodeCompanion.FluentSpreadsheet;
+using CodeCompanion.FluentSpreadsheet.Styles;
+using OfficeOpenXml;
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+var factory = new FluentSpreadsheetFactory();
+var borderDemo_itemCounter = 0;
+factory.New()
+    .AddWorksheet("Border Demo")
+        .OnRange(1, 1, 1, 5)
+        .Merge()
+        .WithValue("Header")
+        .WithFontBold()
+        .WithBorder(SpreadsheetBoxSide.All, SpreadsheetBorderStyle.Thin)
+        .WithHorizontalAlignment(SpreadsheetHorizontalAlignment.Center)
+        .Worksheet
+        .While(() => borderDemo_itemCounter < 10, worksheet => {
+            worksheet
+                .OnRange(2 + borderDemo_itemCounter, 1)
+                    .WithValue(borderDemo_itemCounter + 1)
+                    .WithBorder(SpreadsheetBoxSide.Bottom | SpreadsheetBoxSide.LeftRight, SpreadsheetBorderStyle.Thin);
+            borderDemo_itemCounter++;
+        })
+
+    .SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "test.xlsx"));
